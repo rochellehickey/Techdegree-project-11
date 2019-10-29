865258f961385d09120f55c814169acbbe72cc38
Rochelle Hickey <rochellehickey@gmail.com>
2019-10-28 21:19:22 -0400

    Add build folder

---
+++ build/precache-manifest.15f0bee774d17b5788bc680611bb3730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafce03c50f0db110d394a0fdf01f907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c7443618594f7827031",
+    "url": "/static/css/main.cfc805e4.chunk.css"
+  },
+  {
+    "revision": "192cea8b1db360e40d82",
+    "url": "/static/js/2.5930a871.chunk.js"
+  },
+  {
+    "revision": "3c7443618594f7827031",
+    "url": "/static/js/main.02c8a2f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
